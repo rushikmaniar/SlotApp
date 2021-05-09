1872d7cfbf8c5787660e620de4fa94399d62220b
Rushik Maniar <rushikmaniar107@gmail.com>
2021-05-09 08:14:52 +0530

    added files for release variant

---
+++ app/src/release/java/com/rushik/cowinslotapp/BaseApplication.kt
@@ -0,0 +1,40 @@
+package com.rushik.cowinslotapp
+
+import android.app.Application
+import android.net.ConnectivityManager
+import android.net.Network
+import android.os.Build
+import com.rushik.cowinslotapp.frameworks.AppCache
+import com.rushik.cowinslotapp.frameworks.AppHelper
+
+class BaseApplication : Application() {
+    private lateinit var connectivityManager: ConnectivityManager
+
+    override fun onCreate() {
+        super.onCreate()
+        connectivityManager = getSystemService(ConnectivityManager::class.java)
+
+        registerNetworkListener()
+    }
+
+    private fun registerNetworkListener() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            connectivityManager.registerDefaultNetworkCallback(object : ConnectivityManager.NetworkCallback() {
+                override fun onAvailable(network: Network) {
+                    AppCache.isNetworkAvailableLiveData.postValue(true)
+                }
+
+                override fun onLost(network: Network) {
+                    AppCache.isNetworkAvailableLiveData.postValue(false)
+                    if (AppCache.isServiceRunningLiveData.value == true) {
+                        AppHelper.showNotification(
+                            applicationContext, 123457,
+                            getString(R.string.network_error),
+                            getString(R.string.message_check_your_connection),
+                        )
+                    }
+                }
+            })
+        }
+    }
+}
